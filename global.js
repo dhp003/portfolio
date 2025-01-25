6d643729785f4970ee056fd441ffb08920f833f6
dhp003 <dhp003@ucsd.edu>
2025-01-24 19:48:49 -0800

    changed links

--- global.js
+++ global.js
@@ -38,49 +38,41 @@ if (localStorage.colorScheme) {
   }
 
   let pages = [
-    { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact' },
-    { url: 'resume/', title: 'Resume' },
+    { url: './', title: 'Home' }, 
+    { url: './projects/', title: 'Projects' },
+    { url: './contact/', title: 'Contact' },
+    { url: './resume/', title: 'Resume' },
     { url: 'https://github.com/dhp003', title: 'GitHub' }
   ];
   
   let nav = document.createElement('nav');
   document.body.prepend(nav);
   
-
   const ARE_WE_HOME = document.documentElement.classList.contains('home');
   
   for (let p of pages) {
     let url = p.url;
     const title = p.title;
   
-    if (!ARE_WE_HOME && !url.startsWith('http')) {
-      url = '../' + url;
-    }
-  
-
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
   
-
     if (a.host === location.host && a.pathname === location.pathname) {
       a.classList.add('current');
     }
   
-
     if (a.host !== location.host) {
       a.target = '_blank';
       a.rel = 'noopener noreferrer';
     }
   
-
     nav.append(a);
   }
-
+  
   let navLinks = $$('nav a');
   let currentLink = navLinks.find(
     (a) => a.host === location.host && a.pathname === location.pathname
   );
   currentLink?.classList.add('current');
+  
